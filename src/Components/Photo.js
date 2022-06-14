7bc114384d5cb2b47b8183a473a0a9eae13e9e5e
prajakta1205 <prajakta12wankhede@gmail.com>
2022-06-15 01:09:19 +0530

    style to image

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -5,7 +5,11 @@ const Photo=(props)=>{
     console.log(props);
     return(
         <figure className="figure">
-            <img src={props.listR.imageLink} alt={props.listR.description}/>
+            <img className="photo" src={props.listR.imageLink} alt={props.listR.description}/>
+            <figcaption><p>{props.listR.description}</p></figcaption>
+            <div className="button-container">
+            <button className="remove-button">Remove</button>
+            </div>
         </figure>
     )
 }
