d320719cb75a8cc3add20d7724f6383469d5ada4
prajakta1205 <prajakta12wankhede@gmail.com>
2022-06-15 00:16:18 +0530

    ImageLoad

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -2,8 +2,11 @@
 import React  from "react";
 
 const Photo=(props)=>{
+    console.log(props);
     return(
-        <figure>{props.id}</figure>
+        <figure className="figure">
+            <img src={props.listR.imageLink} alt={props.listR.description}/>
+        </figure>
     )
 }
 
