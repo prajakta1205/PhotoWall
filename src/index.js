d108c7c94a7a897c70ba4109857bdf282b7b9032
prajakta1205 <prajakta12wankhede@gmail.com>
2022-06-18 00:22:13 +0530

    Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import Main from "./Components/Main";
 import './Styles/stylesheet.css';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import rootReducer from './redux/reducer'
+// import Provider from 'react-redux'
+
+const store=createStore(rootReducer);
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
---
+++ src/redux/reducer.js
@@ -0,0 +1,7 @@
+import posts from "../data/posts";
+
+const postReducer= function posts(state=posts,action){
+    return state;
+}
+
+export default postReducer
