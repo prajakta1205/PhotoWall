db0a0e7d3437a90943849cbb7f222ff24b889862
prajakta1205 <prajakta12wankhede@gmail.com>
2022-06-22 23:38:15 +0530

    minor

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -15,6 +15,12 @@ const Photo=(props)=>{
                 props.removePost(props.index)
                 props.history.push('/')
                 }}>Remove</button>
+                <Link className="button" to={`/single/${post.id}`}>
+                    <div className="comment-count">
+                        <div className="speech-bubble"> </div>
+                        {props.comments[post.id] ? props.comments[post.id].length : 0}
+                    </div>
+                </Link>
             </div>
         </figure>
     )
