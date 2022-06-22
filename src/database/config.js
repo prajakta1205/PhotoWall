3d0a3f9e2dcc3a7e6cf557a65339668630c201f7
prajakta1205 <prajakta12wankhede@gmail.com>
2022-06-22 23:56:07 +0530

    add db

---
+++ src/database/config.js
@@ -0,0 +1,13 @@
+// import * as firebase from 'firebase'
+// var config = {
+//  apiKey: "AIzaSyAWZObGQDfGbplDsJ0QXZGfZ22Ph9vlkgc",
+//  authDomain: "photowall-45333.firebaseapp.com",
+//  databaseURL: "https://photowall-45333.firebaseio.com",
+//  projectId: "photowall-45333",
+//  storageBucket: "photowall-45333.appspot.com",
+//  messagingSenderId: "586411564955"
+//  }
+ 
+//  firebase.initializeApp(config)
+//  const database = firebase.database()
+//  export {database}
